774b4446c90364fd636961e4df103cc7d8addac7
Xiaotian Hong <49309571+Xiaotian0726@users.noreply.github.com>
2020-03-11 16:13:09 +0800

    Add files via upload

--- LifestyleApp/app/src/main/java/com/example/lifestyleapp/notepad/NoteEditActivity.java
+++ LifestyleApp/app/src/main/java/com/example/lifestyleapp/notepad/NoteEditActivity.java
@@ -1,10 +1,17 @@
 package com.example.lifestyleapp.notepad;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import com.example.lifestyleapp.R;
@@ -14,17 +21,84 @@ import java.util.Date;
 
 import javax.xml.transform.Result;
 
-public class NoteEditActivity extends AppCompatActivity {
+public class NoteEditActivity extends BaseActivity {
+
+    private Toolbar myToolbar;
 
     private EditText et;
-    private String content;
-    private String time;
+    private String oldContent = "";
+    private String oldTime = "";
+    private int oldTag = 1;
+    private long id = 0;
+
+    private int openMode = 0;
+    private int tag = 1;
+    private boolean tagChange = false;
+
+    public Intent intent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_edit);
+
+        Intent getIntent = getIntent();
+        openMode = getIntent.getIntExtra("mode", 0);
+
+        myToolbar = (Toolbar) findViewById(R.id.myToolbar);
+        if (openMode == 3) {               // 如果是以点击历史笔记的方式进入编辑界面
+            myToolbar.setTitle("笔记内容");
+            setSupportActionBar(myToolbar);  // 显示删除图标
+        } else if (openMode == 4) {        // 如果是新建笔记的方式进入编辑界面
+            myToolbar.setTitle("新建笔记");
+        }
+//        getSupportActionBar().setHomeButtonEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         et = (EditText) findViewById(R.id.et);
+
+        if (openMode == 3) {
+            id = getIntent.getLongExtra("id", 0);
+            oldContent = getIntent.getStringExtra("content");
+            oldTime = getIntent.getStringExtra("time");
+            oldTag = getIntent.getIntExtra("tag", 1);
+            et.setText(oldContent);
+            et.setSelection(oldContent.length());   // 设置光标的位置为文本的末端
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.note_edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete:
+                new AlertDialog.Builder(NoteEditActivity.this)
+                        .setMessage("确认删除此条笔记？")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // mode为2代表以删除历史笔记的方式返回主界面
+                                intent.putExtra("mode", 2);
+                                intent.putExtra("id", id);
+                                setResult(RESULT_OK, intent);
+                                finish();    // 返回笔记主界面
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -32,14 +106,35 @@ public class NoteEditActivity extends AppCompatActivity {
         if (keyCode == KeyEvent.KEYCODE_HOME) {
             return true;
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
-            Intent intent = new Intent();          // 这个intent用户传输信息
-            intent.putExtra("content", et.getText().toString());
-            intent.putExtra("time", dateToStr());
+            autoSetMessage();
             setResult(RESULT_OK, intent);
             finish();
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public void autoSetMessage() {
+        if (openMode == 4) {   // 笔记为新建笔记
+            if (et.getText().toString().length() == 0) {  // 若新建笔记为空，则无需任何操作
+                intent.putExtra("mode", -1);
+            } else {
+                intent.putExtra("mode", 0);
+                intent.putExtra("content", et.getText().toString());
+                intent.putExtra("time", dateToStr());
+                intent.putExtra("tag", tag);
+            }
+        } else {          // 打开一个历史笔记
+            if (et.getText().toString().equals(oldContent) && tagChange == false) {   // 没有作任何修改
+                intent.putExtra("mode", -1);
+            } else {          // 对历史笔记进行了修改
+                intent.putExtra("mode", 1);
+                intent.putExtra("content", et.getText().toString());
+                intent.putExtra("time", dateToStr());
+                intent.putExtra("id", id);
+                intent.putExtra("tag", tag);
+            }
+        }
     }
 
     public String dateToStr(){
--- LifestyleApp/app/src/main/java/com/example/lifestyleapp/notepad/NotepadActivity.java
+++ LifestyleApp/app/src/main/java/com/example/lifestyleapp/notepad/NotepadActivity.java
@@ -1,24 +1,34 @@
 package com.example.lifestyleapp.notepad;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.lifestyleapp.MainActivity;
 import com.example.lifestyleapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NotepadActivity extends AppCompatActivity {
+public class NotepadActivity extends BaseActivity implements AdapterView.OnItemClickListener {
 
     private Context context = this;
+    private Toolbar myToolbar;
 
     private NoteDatabase dbHelper;
     private NoteAdapter adapter;
@@ -32,32 +42,110 @@ public class NotepadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad);
 
+        myToolbar = (Toolbar) findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {  // Toolbar 返回键返回主界面
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotepadActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
         lv = (ListView) findViewById(R.id.lv);
         adapter = new NoteAdapter(getApplicationContext(), noteList);
         refreshListView();
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(this);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NotepadActivity.this, NoteEditActivity.class);
+                intent.putExtra("mode", 4);    // mode=4, 新建笔记
                 startActivityForResult(intent, 0);
             }
         });
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notepad_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.clear:
+                new AlertDialog.Builder(NotepadActivity.this)
+                        .setMessage("确认清空所有笔记？")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dbHelper = new NoteDatabase(context);
+                                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                                // notes是数据库的TABLE_NAME
+                                db.delete("notes", null, null);
+                                db.execSQL("update sqlite_sequence set seq=0 where name='notes'"); // 使id归零
+                                refreshListView();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        String content = data.getStringExtra("content");
-        String time = data.getStringExtra("time");
-        Note note = new Note(content, time, 1);
-        CRUD op = new CRUD(context);
-        op.open();
-        op.addNote(note);
-        op.close();
+        int returnMode;
+        long noteId;
+        returnMode = data.getExtras().getInt("mode", -1);
+        noteId = data.getExtras().getLong("id", 0);
+
+        if (returnMode == 1) {          // 修改了一个历史笔记
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+            Note updatedNote = new Note(content, time, tag);
+            updatedNote.setId(noteId);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.updateNote(updatedNote);
+            op.close();
+        } else if (returnMode == 0) {   // 新建了一个非空笔记
+            String content = data.getExtras().getString("content");
+            String time = data.getExtras().getString("time");
+            int tag = data.getExtras().getInt("tag", 1);
+            Note newNote = new Note(content, time, tag);
+            CRUD op = new CRUD(context);
+            op.open();
+            op.addNote(newNote);
+            op.close();
+        } else if (returnMode == 2) {   // 某一条历史笔记被删除
+            long idOfDeletedNote = data.getExtras().getLong("id");
+            CRUD op = new CRUD(context);
+            op.open();
+            Note tempNote = new Note();
+            tempNote.setId(idOfDeletedNote);
+            op.removeNote(tempNote);
+            op.close();
+        }
+
         refreshListView();
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void refreshListView() {
@@ -69,5 +157,21 @@ public class NotepadActivity extends AppCompatActivity {
         noteList.addAll(op.getAllNotes());
         op.close();
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.lv:
+                Note currNote = (Note) parent.getItemAtPosition(position);
+                Intent intent = new Intent(NotepadActivity.this, NoteEditActivity.class);
+                intent.putExtra("content", currNote.getContent());
+                intent.putExtra("id", currNote.getId());
+                intent.putExtra("time", currNote.getTime());
+                intent.putExtra("mode", 3);    // openMode=3，点击一个历史笔记进入EditActivity
+                intent.putExtra("tag", currNote.getTag());
+                startActivityForResult(intent, 1);
+                break;
+        }
     }
 }
